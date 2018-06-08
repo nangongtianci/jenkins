2e89156867c18edb2d0c7ea7b9f9c0d50f4eb27c
nangongtianci <675886926@qq.com>
2018-06-08 14:46:43 +0800

    first commit

---
+++ src/main/java/com/jenkins/ylw/YlwApplication.java
@@ -0,0 +1,12 @@
+package com.jenkins.ylw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YlwApplication.class, args);
+	}
+}
